437a58f69fbd8a97ba75194b72089b52cbd54662
GC_1A_19_MikuniRyota <K020G1101@g.neec.ac.jp>
2020-12-03 12:09:55 +0900

    d

--- Git_Test_Project.cpp
+++ Git_Test_Project.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello VisualStdio Git!!!\n";Hello VisualStdio Git!!!
+    std::cout << "Hello Cat!!!\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
